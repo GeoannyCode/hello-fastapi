f9c897400be7d7b611fcee89c959915646a0cd18
geovannydev <diegobracero@hotmail.com>
2022-12-06 21:59:22 -0500

    validations_query

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 #FastAPI 
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -23,7 +23,18 @@ def home():
     return{"Hello": "World"}
 
 # Request and response Body
+# (...) Tiene que ser obligatorio
 
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+
+#Validations: Query Parameters
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_Length=1,max_Length=50),
+    age: str = Query(...)
+
+):
+    return {name: age}
