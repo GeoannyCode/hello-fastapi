20c3549ac9e5ccc039cc63ad663877e1b41f8ce5
geovannydev <diegobracero@hotmail.com>
2023-01-07 14:26:32 -0500

    remove_duplicates

--- main.py
+++ main.py
@@ -37,12 +37,15 @@ class Location(BaseModel):
     #         }
     #     }
 
-class Person(BaseModel):
+class PersonBase(BaseModel):
     first_name: str = Field(..., min_length=1, max_length = 50)
     last_name: str = Field(..., min_length=1, max_length = 50)
     age: int = Field(..., gt=0, le=115)
     hair_color: Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = Field(default=None)
+
+class Person(PersonBase):
+    
     password: str = Field(..., min_length=8)
 
     class Config:
@@ -57,12 +60,8 @@ class Person(BaseModel):
             }
         }
 
-class PersonOut(BaseModel):
-    first_name: str = Field(..., min_length=1, max_length = 50)
-    last_name: str = Field(..., min_length=1, max_length = 50)
-    age: int = Field(..., gt=0, le=115)
-    hair_color: Optional[HairColor] = Field(default=None)
-    is_married: Optional[bool] = Field(default=None)
+class PersonOut(PersonBase):
+    pass
 
 @app.get("/") #path decorator
 def home():
